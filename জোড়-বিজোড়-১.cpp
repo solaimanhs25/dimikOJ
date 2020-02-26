05693a6c2fd15189e874dbf46da881ad1a5e7eed
Solaiman Hossain <61455333+solaimanhs25@users.noreply.github.com>
2020-02-26 18:40:29 +0600

    Create  জোড়-বিজোড়-১.cpp

---
+++ জোড়-বিজোড়-১.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int num;
+        cin>>num;
+
+        if(num%2==0)
+            cout<<"even"<<endl;
+        else
+            cout<<"odd"<<endl;
+    }
+
+    return 0;
+}
+
